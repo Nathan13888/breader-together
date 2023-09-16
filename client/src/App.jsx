504e5c58ea0b0f43b723cf39c39203b216922e6b
ricsign <ricsign123@gmail.com>
2023-09-16 10:56:31 -0400

    App and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useContext } from "react";
+
+import UserProvider from "./context/userProvider";
+import UserContext from "./context/userContext";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import LoggedRoute from "./routes/LoggedRoute";
+import PublicRoute from "./routes/PublicRoute";
+
+import Login from "./components/Login/Login";
+import HomePage from "./components/HomePage/HomePage";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { user, setUser } = useContext(UserContext);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PublicRoute component={Login} />} />
+          <Route path="/home" element={<LoggedRoute component={HomePage} />} />
+        </Routes>
+      </Router>
+    </UserProvider>
+  );
 }
 
-export default App
+export default App;
